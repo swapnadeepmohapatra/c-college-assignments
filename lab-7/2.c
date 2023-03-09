5de61df1a6a54b48a90424fbca5a02bf9100db7e
Dipan Dash <117461629+imddash@users.noreply.github.com>
2023-03-09 20:22:45 +0530

    Fix/hack of Bugs
    Enjoy my friends just 1 year of coding.

--- lab-7/2.c
+++ lab-7/2.c
@@ -2,40 +2,30 @@
 
 #include <stdio.h>
 
-int main()
-{
-    int marks;
-    printf("Enter your marks: ");
-    scanf("%d", &marks);
+int main() {
+   int marks;
 
-    if (marks >= 90)
-    {
-        printf("Your grade is 0.");
-    }
-    else if (marks >= 80)
-    {
-        printf("Your grade is E.");
-    }
-    else if (marks >= 70)
-    {
-        printf("Your grade is A.");
-    }
-    else if (marks >= 60)
-    {
-        printf("Your grade is B.");
-    }
-    else if (marks >= 50)
-    {
-        printf("Your grade is C.");
-    }
-    else if (marks >= 40)
-    {
-        printf("Your grade is D.");
-    }
-    else
-    {
-        printf("Your grade is E.");
-    }
+   printf("Enter your marks: ");
+   scanf("%d", &marks);
 
-    return 0;
+   if (marks >= 90 && marks <= 100) {
+      printf("Grade A");
+   }
+   else if (marks >= 80 && marks < 90) {
+      printf("Grade B");
+   }
+   else if (marks >= 70 && marks < 80) {
+      printf("Grade C");
+   }
+   else if (marks >= 60 && marks < 70) {
+      printf("Grade D");
+   }
+   else if (marks >= 0 && marks < 60) {
+      printf("Grade F");
+   }
+   else {
+      printf("Invalid marks entered!");
+   }
+
+   return 0;
 }
