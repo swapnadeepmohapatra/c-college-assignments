8b83ce6d0edd4b682e71e8e0cc065a68ba861dc0
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2023-03-10 14:45:11 +0530

    Revert "Fix/hack of Bugs."

--- lab-7/2.c
+++ lab-7/2.c
@@ -2,30 +2,40 @@
 
 #include <stdio.h>
 
-int main() {
-   int marks;
+int main()
+{
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
 
-   printf("Enter your marks: ");
-   scanf("%d", &marks);
+    if (marks >= 90)
+    {
+        printf("Your grade is 0.");
+    }
+    else if (marks >= 80)
+    {
+        printf("Your grade is E.");
+    }
+    else if (marks >= 70)
+    {
+        printf("Your grade is A.");
+    }
+    else if (marks >= 60)
+    {
+        printf("Your grade is B.");
+    }
+    else if (marks >= 50)
+    {
+        printf("Your grade is C.");
+    }
+    else if (marks >= 40)
+    {
+        printf("Your grade is D.");
+    }
+    else
+    {
+        printf("Your grade is E.");
+    }
 
-   if (marks >= 90 && marks <= 100) {
-      printf("Grade A");
-   }
-   else if (marks >= 80 && marks < 90) {
-      printf("Grade B");
-   }
-   else if (marks >= 70 && marks < 80) {
-      printf("Grade C");
-   }
-   else if (marks >= 60 && marks < 70) {
-      printf("Grade D");
-   }
-   else if (marks >= 0 && marks < 60) {
-      printf("Grade F");
-   }
-   else {
-      printf("Invalid marks entered!");
-   }
-
-   return 0;
+    return 0;
 }
